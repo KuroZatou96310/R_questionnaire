589b9837abc77a77999ee3cdcf4b51289da2ba3c
KuroZatou96310 <hukurouc6@gmail.com>
2026-06-15 22:12:44 +0900

    Rでデータベースの中身を見るファイルを作成。

---
+++ Rでデータベースの中身を見る.R
@@ -0,0 +1,38 @@
+
+# Linux側からRでデータベースの中身を見る。
+# コンソールにコード入力して確認して。
+
+library(DBI)
+library(RSQLite)
+
+con <- dbConnect(
+    SQLite(),
+    "/srv/shiny-server/enquete_app_data/questionnaire.db"
+)
+
+
+# 文字コード変更
+Sys.setlocale("LC_CTYPE", "C.UTF-8")
+
+
+# テーブル一覧
+dbListTables(con)
+
+# テーブルの中身を確認
+dbReadTable(con, "answers")
+
+# クエリを実行して、テーブルの中身を確認
+dbGetQuery(con, "SELECT * FROM answers LIMIT 10")
+
+# テーブルの構造を確認
+dbGetQuery(con, "PRAGMA table_info(answers)")
+
+
+● Webサイトから送信された回答が、データベースへ正常に保存されることを確認した。
+
+
+
+
+
+# データベース接続を閉じる
+dbDisconnect(con)
